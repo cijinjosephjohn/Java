ff85ad79855a6438017ce69bce001876872ee46e
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2021-05-26 14:59:01 +0530

    First Program

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+class FirstProgram {
+	public static void main(String args[]){
+		System.out.println("NAME  	MAIL-ID 		EMPLOYEE-CODE	PHONE ");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("Umesh 	umesh@cse		p03161		257684742");
+		System.out.println("Sali 	sali@gcse		p03160		235223830");
+		System.out.println("Rim 	rim@cse			p03163		242532552");
+		System.out.println("John 	john@cse		p03525		223524536");
+		System.out.println("Jis 	jis@cse			p03352		253245830");	
+	}
+}
