86728745a9ccfe06910e663a2b78a7a77e56b803
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2021-05-27 15:24:57 +0530

    Added Program

---
+++ SixthProgram.java
@@ -0,0 +1,14 @@
+public class SixthProgram{
+	public static void main(String args[]){
+		String[] a= {"Charles","Lydia","Charlotte","Kitty","Mary"};
+		int[] b={32,41,30,40,37};
+		int i=0;
+		System.out.println("\n");
+		while(i<5){
+			if(b[i]>35){
+				System.out.println("Name : "+a[i]+"   Mark : "+b[i]+"\n");
+			}
+			i++;
+		}
+	}
+}
