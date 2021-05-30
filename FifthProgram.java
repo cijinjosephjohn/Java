92cbce4d4ca8d8fd6d758dbda3825e95d7ead469
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2021-05-30 12:48:09 +0530

    Java Program

---
+++ FifthProgram.java
@@ -0,0 +1,27 @@
+public class FiveProgram{
+	public static void main(String args[]){
+		String[] a= {"Elizabeth","Darcy","Bennet","Collins","Jane"};
+		int[] b={40,42,30,20,36};
+		if(b[0]>35){
+			System.out.println(" \n");
+			System.out.println("Name : " +a[0]+"  Mark :"+b[0]);
+			System.out.println("\n");
+		}
+		if(b[1]>35){
+			System.out.println("Name : " +a[1]+"  Mark :"+b[1]);
+			System.out.println("\n");
+		}
+		if(b[2]>35){
+			System.out.println("Name : " +a[2]+"  Mark :"+b[2]);
+			System.out.println("\n");
+		}
+		if(b[3]>35){
+			System.out.println("Name : " +a[3]+"  Mark :"+b[3]);
+			System.out.println("\n");
+		}
+		if(b[4]>35){
+			System.out.println("Name : " +a[4]+"  Mark :"+b[4]);
+			System.out.println("\n");
+		}			
+	}
+}
